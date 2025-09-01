edae726a8418ba200d2856daa805ca3cf86dc6fc
FathirThoriq <fathirthoriq9@gmail.com>
2025-09-01 14:09:40 +0700

    add login page

---
+++ app/src/main/java/com/example/hijau/mainactivity.java
@@ -0,0 +1,12 @@
+package com.example.hijau;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class mainactivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login); // menampilkan login.xml
+    }
+}
