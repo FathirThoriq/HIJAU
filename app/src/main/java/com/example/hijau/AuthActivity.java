f0a7030e684d117c7d4c5496108e801b35f910b0
FathirThoriq <fathirthoriq9@gmail.com>
2025-09-05 10:13:15 +0700

    update main ke auth

---
+++ app/src/main/java/com/example/hijau/AuthActivity.java
@@ -0,0 +1,18 @@
+package com.example.hijau;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth); // activity_auth punya FrameLayout
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new LoginFragment())
+                    .commit();
+        }
+    }
+}
